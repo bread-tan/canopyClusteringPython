1df32240aa444df0b50dd24d9883adc4bac9b5a8
Archit Shukla <archishukla2@gmail.com>
2014-03-03 18:15:11 +0530

    Fixed a bug in reducerStg1
    Signed-off-by: Archit Shukla <archishukla2@gmail.com>

--- reducerStg1.py
+++ reducerStg1.py
@@ -9,20 +9,19 @@ for line in sys.stdin:
 	(kev,value) = line.split("\t")
 	dp = DataPoint.DataPoint(value.strip())
 
-	if len(canopyCenters) == False:
+	if len(canopyCenters) == 0:
 		canopyCenters.append(dp)
 
 	else:
 		insert = True
 
 		for center in canopyCenters:
-			insert = dp.checkT2(center)
-			if insert == False:
+			if dp.checkT2(center):
+				insert = False
 				break
 
 		if insert == True:
 			canopyCenters.append(dp)
-canopyCenters.append(dp)
 
 for canopyCenter in canopyCenters:
 	print("1\t" + canopyCenter.toString())
