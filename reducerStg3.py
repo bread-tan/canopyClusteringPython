219e8a8ec2223e504eebd52530ff8fcf5f0ab421
sheraaz-jason <sheraaz.jason@gmail.com>
2014-03-07 00:10:19 +0530

    Fixed some bugs with reducerStg3.py. Need to iron out some chinks.

--- reducerStg3.py
+++ reducerStg3.py
@@ -25,6 +25,7 @@ for line in sys.stdin:
 		yearSum = yearSum + dataPoints.year
 		tempSum = tempSum + dataPoints.temp
 		count = count + 1
+		continue
 		#print (str(yearSum)+ "\t" + str(tempSum) + "\t" + str(count))
 	
 	if prevKey != kCentroid:
@@ -32,11 +33,21 @@ for line in sys.stdin:
 		avgTemp = tempSum / count
 		
 
-		print ("1\t"+str(avgYear)+","+str(avgTemp)+"\tCount: ")
+		print("Year Sum: " + str(yearSum) + "\t\tAvg Year: " + str(avgYear))
+		print("Temp Sum: " +str(tempSum) + "\t\tAvg Temp: " + str(avgTemp))
+		print("Count: " + str(count))
+		print("K Centroid:" + kCentroid.toString()+"\n")
+
+
+		print ("1\t"+str(avgYear)+","+str(avgTemp))
 
 		count = 1
 		yearSum = dataPoints.year
-		tempSum = dataPoints.tempSum
+		tempSum = dataPoints.temp
 		prevKey = kCentroid
 
-#print("Output:\t1\t" + str(avgYear) + "," + str(avgTemp))
+print ("1\t"+str(avgYear)+","+str(avgTemp))		
+print("Year Sum: " + str(yearSum) + "\t\tAvg Year: " + str(avgYear))
+print("Temp Sum: " +str(tempSum) + "\t\tAvg Temp: " + str(avgTemp))
+print("Count: " + str(count))
+print("K Centroid:" + kCentroid.toString())
