0cf318381d9aaea89a578add4bf8ae06844859a0
sheraaz-jason <sheraaz.jason@gmail.com>
2014-03-07 01:59:23 +0530

    Added reducerStg4.py, fixed shell script to run it.

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@
 cat dataPoints.txt | ./mapperStg1.py | sort | ./reducerStg1.py > canopyCenters.txt
 
 # Stage 2 & 3
-cat dataPoints.txt | ./mapperStg2.py | sort | ./reducerStg2.py | ./mapperStg3.py | sort | ./reducerStg3.py | ./mapperStg4.py | sort
-
+cat dataPoints.txt | ./mapperStg2.py | sort | ./reducerStg2.py | ./mapperStg3.py | sort | ./reducerStg3.py | ./mapperStg4.py | sort | ./reducerStg4.py
