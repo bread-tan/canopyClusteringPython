318025d4173c8d09af6b3ce44ad40d79eff1e4c3
sheraaz-jason <sheraaz.jason@gmail.com>
2014-03-23 04:19:39 +0530

    Various changes: Fixed iterative step of phase 3 using runtest.sh To fix: 1) Stage 3 from reducing the number of centroids

---
+++ canopyClusteringPython/mapperStg2.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import sys
+import DataPoint
+
+#file = open("dataPoints.txt","r")
+canopyCentersFile = open("canopyCenters.txt","r")
+canopyCenters = []
+canopys = []
+#DataPoints = []
+
+#taking data from the std input
+
+
+for line in canopyCentersFile:
+	(key,value) = line.split("\t")
+	dp = DataPoint.DataPoint(value.strip())
+	canopyCenters.append(dp)
+#canopyCenters.append(dp)
+
+#for centroid in canopyCenters:
+#	wfile.write(centroid.toString() + "\n")
+#wfile.write(centroid.toString())
+
+for line in sys.stdin:
+	
+	dp = DataPoint.DataPoint(line.strip())
+# 	#DataPoints.append(dp)
+# 	#DataPoints.append(dp)
+
+# """
+# for dp in DataPoints:
+# 	print(dp.toString())
+# for centroid in canopyCenters:
+# 	print(centroid.toString())
+# """
+
+	insert = True
+#	for dataPoint in DataPoints: 
+ 	for canopyCenter in canopyCenters:	
+ 		if dp.checkT1(canopyCenter):
+ 			print(canopyCenter.toString() + "\t" + dp.toString())
---
+++ runtest.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+`./test.py`
+if [ $? -eq 0 ]; then
+	echo "Works"
+fi
